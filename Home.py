1c2b9bd73d03ba4228d9519ad7380129126038af
msengia-tech <martinho@dugraf.com.br>
2026-03-04 16:28:12 -0300

    Commit inicial: inserir arquivos inicias

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+from header import show_header
+
+show_header()  # Exibe o cabeçalho padrão
---
+++ header.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def show_header():
+    # --- Configuração da Página ---
+    # Define o título da página, o ícone e o layout para ocupar a largura inteira.
+    st.set_page_config(
+        page_title="Dugraf - Dashboard",
+        page_icon="📊",
+        layout="wide",
+    )
+    col1, col2 = st.columns([5, 8])
+    with col1:
+        st.image("assets/logo_dugraf_branco.png", width=150)  # controla o tamanho aqui
+    with col2:
+        st.markdown(
+            "<h1 style='margin:0; margin-top:50px; padding-top:8px;'>DASHBOARD DUGRAF - DIRETORIA</h1>",
+            unsafe_allow_html=True,
+        )
